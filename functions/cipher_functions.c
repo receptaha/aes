923ba3bd4fcb2ba7c4c4551f2fae273a46460896
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-12-11 01:26:07 +0300

    left rotation, rc and g functions added to cipher_functions.c / .h

--- functions/cipher_functions.c
+++ functions/cipher_functions.c
@@ -10,7 +10,7 @@ void validate_base_key(const char* base_key) {
     unsigned long long len = strlen(base_key);
     u_int8_t valid = 0;
 
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < sizeof(KEY_SIZES) / sizeof(KEY_SIZES[0]); i++){
         if (len * 8 == KEY_SIZES[i]) valid = 1;
     }
 
@@ -26,7 +26,7 @@ u_int32_t** key_expension(const char* base_key) {
     int round_count = 0;
 
     // Round count setting.
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < sizeof(KEY_SIZES) / sizeof(KEY_SIZES[0]); i++) {
         if (base_key_len * 8 == KEY_SIZES[i]) round_count = ROUNDS[i];
     }
 
@@ -35,7 +35,7 @@ u_int32_t** key_expension(const char* base_key) {
         exit(-1);
     }
 
-    u_int32_t** round_keys = malloc(sizeof(u_int32_t*) * round_count);
+    u_int32_t** round_keys = malloc(sizeof(u_int32_t*) * (round_count + 1));
     if (round_keys == NULL) {
         printf("Round keys cannot allocated");
         exit(-1);
@@ -54,22 +54,55 @@ u_int32_t** key_expension(const char* base_key) {
     }
 
     // Other keys are placing to other rows of the rounds keys
-    // for (int i = 1; i <= round_count; i++) {
-    //     for (int j = 0; j < word_count_per_round; j++) {
-    //         if (j == 0) {
-    //             round_keys[i][j] = round_keys[i-1][0] | g(round_keys[i-1][3]);
-    //         }else {
-    //             round_keys[i][j] = round_keys[i][j-1] | round_keys[i-1][j];
-    //         }
-    //     }
-    // }
+    for (int i = 1; i <= round_count; i++) {
+        for (int j = 0; j < word_count_per_round; j++) {
+            if (j == 0) {
+                round_keys[i][j] = round_keys[i-1][0] ^ g(round_keys[i-1][word_count_per_round-1], i);
+            }else {
+                round_keys[i][j] = round_keys[i][j-1] ^ round_keys[i-1][j];
+            }
+        }
+    }
     return round_keys;
+}
+
+u_int32_t left_rotation(u_int32_t word, int byte_count) {
+    return (word << (8 * byte_count)) | (word >> 8 * (sizeof(u_int32_t) - byte_count));
+}
+
+u_int8_t rc(u_int8_t i) {
+    if (i == 1) return 0x01;
+
+    u_int8_t prev_rc = rc(i-1);
+    u_int8_t result = prev_rc << 1;
+
+    if (prev_rc & 0x80) { // if the last bit is 1
+       result ^= irreducible_polynom;
+    }
+
+    return result;
+}
+
+u_int32_t g(u_int32_t word, u_int8_t round_number) {
+
+    // 1-byte circular left rotation
+    u_int32_t left_rotated_word = left_rotation(word, 1);
+    u_int32_t sub_byted_word = 0;
+    // SubByte each byte of the left rotated word
+    for (int i = 0; i < 4; i++) {
+        u_int8_t byte = left_rotated_word << (8 * i);
+        byte >>= 24;
+        sub_byted_word = sub_byted_word | ((u_int32_t) subByte(byte) << (24 - 8 * i));
+    }
+    // xor subByted word and round constant and return it
+    return sub_byted_word ^ ((u_int32_t ) rc(round_number) << 24);
 }
 
 u_int8_t subByte(u_int8_t byte) {
     u_int8_t row = byte >> 4;
     u_int8_t column = byte << 4;
-    return s_box_encrypt[row][column >> 4];
+    column >>= 4;
+    return s_box_encrypt[row][column];
 }
 
 
--- headers/cipher_functions.h
+++ headers/cipher_functions.h
@@ -4,8 +4,11 @@
 #include <sys/_types/_u_int8_t.h>
 
 void validate_base_key(const char* base_key);
-u_int8_t subByte(u_int8_t byte);
 u_int32_t** key_expension(const char* base_key);
+u_int32_t left_rotation(u_int32_t word, int byte_count);
+u_int8_t rc(u_int8_t i);
+u_int32_t g(u_int32_t word, u_int8_t round_number);
+u_int8_t subByte(u_int8_t byte);
 
 
 #endif
