2956faf9dc5c403fc03d51942ceb0dfef616a21f
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-12-10 17:08:33 +0300

    cipher functions .c / .h added.

---
+++ functions/cipher_functions.c
@@ -0,0 +1,19 @@
+#include "../headers/cipher_functions.h"
+#include "../headers/constants.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void validate_base_key(const char* base_key) {
+    unsigned long long len = strlen(base_key);
+    u_int8_t valid = 0;
+
+    for (int i = 0; i < 3; i++){
+        if (len * 8 == KEY_SIZES[i]) valid = 1;
+    }
+
+    if (valid == 0) {
+        printf("Invalid key size");
+        exit(-1);
+    }
+}
---
+++ headers/cipher_functions.h
@@ -0,0 +1,6 @@
+#ifndef CIPHER_FUNCTIONS_H
+#define CIPHER_FUNCTIONS_H
+
+void validate_base_key(const char* base_key);
+
+#endif
