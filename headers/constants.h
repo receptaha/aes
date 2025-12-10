b2c047752039e34661e1cd8906216a3682b2e3d5
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-12-11 02:05:15 +0300

    constants updated

--- headers/constants.h
+++ headers/constants.h
@@ -1,10 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int BLOCK_SIZE;
-const int KEY_SIZES[3];
-const int ROUNDS[3];
-const char* BASE_KEY;
-const u_int8_t irreducible_polynom = 0x1B;
-const u_int8_t s_box_encrypt[16][16];
+extern const int BLOCK_SIZE;
+extern const int KEY_SIZES[3];
+extern const int ROUNDS[3];
+extern const char* BASE_KEY;
+extern const u_int8_t irreducible_polynom;
+extern const u_int8_t s_box_encrypt[16][16];
 #endif
