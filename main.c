be555c674d8ba879c8539d0e46ecb07f9cdf1920
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-12-11 18:00:25 +0300

    main updated for print first key of rounds keys as state array

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 
 int main() {
     validate_base_key(BASE_KEY);
-    u_int32_t** round_keys = key_expension(BASE_KEY);
+    u_int8_t*** round_keys = key_expension(BASE_KEY);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%c\t", round_keys[0][i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
