f5981f0b08486baf0afd31c8d0dac0b6e2f65e9f
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-12-11 17:59:48 +0300

    find_file_or_fail method created in file_functions .c / .h

--- functions/file_functions.c
+++ functions/file_functions.c
@@ -1,1 +1,13 @@
 #include "../headers/file_functions.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* find_file_or_fail(char* file) {
+    FILE* f = fopen(file, "rb");
+    if (f == NULL) {
+        printf("\nFile cannot opened");
+        exit(-1);
+    }
+    return f;
+}
--- headers/file_functions.h
+++ headers/file_functions.h
@@ -1,4 +1,7 @@
 #ifndef FILE_FUNCTIONS_H
 #define FILE_FUNCTIONS_H
+#include <stdio.h>
+
+FILE* find_file_or_fail(char* file);
 
 #endif
